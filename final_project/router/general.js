9ccaf649f23f09305213f21457ffbd4908683d97
Myrto Tzelisi <myrto.tzelisi@gmail.com>
2024-09-26 08:08:21 -0400

    task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,7 +52,9 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = parseInt(req.params.isbn);
+  let book_isbn = books[isbn]; 
+  return res.status(200).json(book_isbn.reviews);
 });
 
 module.exports.general = public_users;

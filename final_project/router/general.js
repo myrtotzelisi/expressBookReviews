60f272f2c2c1ed0535edfd7f47672522608f4d53
Myrto Tzelisi <myrto.tzelisi@gmail.com>
2024-09-26 08:35:19 -0400

    task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,27 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist =(username) => {
+    let existingUser = users.filter((user) =>
+        {
+            return user.username === username;
+        });
+    return existingUser.length>0;
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Please provide username and password" });
 });
 
 // Get the book list available in the shop
